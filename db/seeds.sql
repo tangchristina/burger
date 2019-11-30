87da884d3533dd284737d00adbe1412756869f3e
Christina Tang <christinatangerine@hotmail.com>
2019-11-30 15:21:41 -0500

    table dynamically updates with new burger added

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Portobello Mushroom', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheese', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Wild Salmon', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Black Bean', false);
