ee856e56236428aa9ca62b97ffe7ae4069c44233
Christina Tang <christinatangerine@hotmail.com>
2019-12-02 18:53:28 -0800

    everything is working except for delete button

--- config/orm.js
+++ config/orm.js
@@ -87,10 +87,24 @@ var orm = {
 
       cb(result);
     });
-  }
+  },
+      //Delete function/query
+      delete: function(table, condition, cb) {
+        var queryString = "DELETE FROM " + table;
+        queryString += " WHERE ";
+        queryString += condition;
+    
+        connection.query(queryString, function(err, result) {
+          if (err) {
+            throw err;
+          }
+    
+          cb(result);
+        });
+      }
 };
 
-// Export the orm object for the model (cat.js).
+// Export the orm object for the model (burger.js).
 module.exports = orm;
 
 
--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -50,6 +50,22 @@ router.get("/index", function(req, res) {
     );
   });
 
+//   router.delete("/burgers/delete/:id", function(req, res) {
+//     var condition = "id = " + req.params.id;
+  
+//     burger.delete(condition, function(result) {
+//           if (result.changedRows === 0) {
+//             // If no rows were changed, then the ID must not exist, so 404
+//             return res.status(404).end();
+//           }
+//           res.redirect('/index');
+//           res.status(200).end();
+          
+    
+//         }
+//       );
+//   });
+
 
 
 // Export routes for server.js to use.
